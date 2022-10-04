45dabac8ddbfb9a3370afe29bdf89474c3001db1
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-10-04 17:20:30 -0400

    added lualine

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,32 @@
+local fn = vim.fn
+local cmd = vim.cmd
+
+-- bootstrap Packer
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+    -- packer package manager
+    use 'wbthomason/packer.nvim'
+
+    -- lualine status bar
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {'kyazdani42/nvim-web-devicons', opt = true},
+        config = function() require('plugins.lualine') end
+    }
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,7 @@
+require('lualine').setup({
+    options = {
+        theme = 'gruvbox',
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '|', right = '|' }
+    }
+})
