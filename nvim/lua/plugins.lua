99bc911f9ce930ba03ee00175a07d94ce30cb16c
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-10-16 14:07:24 -0400

    autocompletion

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -19,6 +19,21 @@ return require('packer').startup(function(use)
     -- packer package manager
     use {'wbthomason/packer.nvim', opt = true}
 
+    -- autocompletion
+    use {'hrsh7th/nvim-cmp',
+        config = function () require('plugins.cmp') end
+    }
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'saadparwaiz1/cmp_luasnip'
+    use {
+        'windwp/nvim-autopairs',
+        config = function() require('nvim-autopairs').setup {} end
+    }
+    
+    -- snippets
+    use 'L3MON4D3/LuaSnip' --snippet engine
     -- lsp
     use {
         'neovim/nvim-lspconfig',
