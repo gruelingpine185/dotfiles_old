a7270aad536e110079032eb0020774d5fb0c53e6
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2023-08-14 12:48:01 -0400

    markdown preview commands now show up

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -92,10 +92,12 @@ return require('packer').startup(function(use)
         end
     }
 
-    use {
+    use({
         'iamcco/markdown-preview.nvim',
-        run = function() vim.fn['mkdp#util#install']() end
-    }
+        run = "cd app && npm install",
+        setup = function() vim.g.mkdp_filetypes = {'markdown'} end,
+        ft = {'markdown'},
+    })
 
     -- colorschemes
     use {
